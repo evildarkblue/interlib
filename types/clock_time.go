1db0776d69da73913175b42b3ff3d5b3420b42c9
Chen Yan Jui <ch.focke@gmail.com>
2024-04-23 15:50:36 +0800

    feat: add clock_time type

---
+++ types/clock_time.go
@@ -0,0 +1,71 @@
+package types
+
+import (
+	"fmt"
+	"time"
+)
+
+func NewClockTime(h, m, s int) *ClockTime {
+	c := ClockTime(time.Date(1911, 1, 1, h, m, s, 0, time.Now().Location()))
+	return &c
+}
+
+type ClockTime time.Time
+
+const clockTimeFormat = "15:04:05Z07:00"
+
+func (ct ClockTime) String() string {
+	return time.Time(ct).Format(clockTimeFormat)
+}
+
+func (ct ClockTime) Equal(tt ClockTime) bool {
+	t1 := time.Time(ct)
+	t2 := time.Time(tt)
+	return (t1.Hour() == t2.Hour() && t1.Second() == t2.Second())
+}
+
+func (ct ClockTime) Sub(tt time.Time) int {
+	t := time.Time(ct)
+	t = t.In(tt.Location())
+	totalMins := (t.Hour() - tt.Hour()) * 60
+	totalMins += t.Minute() - tt.Minute()
+	return t.Second() - tt.Second() + totalMins*60
+}
+
+func (ct ClockTime) MarshalYAML() (interface{}, error) {
+	tt := time.Time(ct)
+	return tt.Format(clockTimeFormat), nil
+}
+
+func (ct *ClockTime) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var str string
+	err := unmarshal(&str)
+	if err != nil {
+		return err
+	}
+	t, err := time.Parse(clockTimeFormat, str)
+	if err != nil {
+		return err
+	}
+	*ct = ClockTime(t)
+	return nil
+}
+
+func (ct *ClockTime) UnmarshalJSON(b []byte) error {
+	str := string(b)
+	result := str[1 : len(str)-1]
+	if len(result) == 0 {
+		return nil
+	}
+	t, err := time.Parse(clockTimeFormat, result)
+	if err != nil {
+		return err
+	}
+	*ct = ClockTime(t)
+	return nil
+}
+
+func (ct *ClockTime) MarshalJSON() ([]byte, error) {
+	tt := time.Time(*ct)
+	return []byte(fmt.Sprintf("\"%s\"", tt.Format(clockTimeFormat))), nil
+}
